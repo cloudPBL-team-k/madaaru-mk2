ff4d990aa843343b3baa0db2bf9294c1065ecc11
nakakkkkkkknn <nakashima.kenji124@mail.kyutech.jp>
2018-01-05 20:49:04 +0900

    1st VS Solution commit

---
+++ madaaru-mk2/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+
+namespace madaarumk2 {
+    public partial class App : Application {
+        public App() {
+            //InitializeComponent();
+
+            //MainPage = new madaaru_mk2Page();
+            MainPage = new NavigationPage(new madaaru_mk2Page());
+        }
+
+        protected override void OnStart() {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep() {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume() {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ madaaru-mk2/madaaru-mk2Page.xaml.cs
@@ -0,0 +1,9 @@
+﻿using Xamarin.Forms;
+
+namespace madaarumk2 {
+    public partial class madaaru_mk2Page : ContentPage {
+        public madaaru_mk2Page() {
+            InitializeComponent();
+        }
+    }
+}
