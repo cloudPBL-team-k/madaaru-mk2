6355155e733e10d4273f093cdce71d6fa00dbf2c
satou <n231041h@mail.kyutech.jp>
2018-01-10 19:12:13 +0900

    リスト表示（完成）

--- madaaru-mk2/ServerInfo.cs
+++ madaaru-mk2/ServerInfo.cs
@@ -1,4 +1,4 @@
 ﻿public static class ServerInfo {
-    //public static string url = "http://www.samidare.blue:3000";
-    public static string url = "http://203.104.200.194:3000";
+    public static string url = "http://www.samidare.blue:3000";
+    //public static string url = "http://203.104.200.194:3000";
 }
