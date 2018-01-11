1a3c3c92c38b1803ed898b451d01b20fe9df0c80
bushido-00144 <bushdo.00144@gmail.com>
2018-01-11 16:32:56 +0900

    コンフリクトによって発生したバグを解消

--- madaaru-mk2/BuyThingListPage.xaml.cs
+++ madaaru-mk2/BuyThingListPage.xaml.cs
@@ -6,38 +6,39 @@ using Xamarin.Forms;
 using ZXing.Net.Mobile.Forms;
 using System.Threading.Tasks;
 
-namespace madaarumk2 {
-    public partial class BuyThingListPage : ContentPage {
-        public BuyThingListPage() {
+namespace madaarumk2
+{
+    public partial class BuyThingListPage : ContentPage
+    {
+        public BuyThingListPage()
+        {
             InitializeComponent();
             setBuyList();
         }
 
         //Listを取得してセットする処理を書く
-        async Task setBuyList(){
+        async Task setBuyList()
+        {
             User user = (User)Application.Current.Properties["user"];
             int userId = user.id;
             GetObjects go = new GetObjects();
             string jsonString = await go.GetBuythingInfo(userId);
-            
-        async void addBtnClicked(object sender, EventArgs s) {
 
-            DependencyService.Get<IMyFormsToast>().Show("NavigationStuck :" + Navigation.NavigationStack.Count);
-
-            string nextPageResult = await DisplayActionSheet(
-                    "追加方法を選ぶ", "閉じる", "キャンセル",
-                    new string[] { "リストから", "スキャンする" });
+            List<Buy_thing> buythingInfo = go.GetbuythingObjectFromJson(jsonString);
+            Dictionary<string, string> item = new Dictionary<string, string>();
 
             var cell = new DataTemplate(typeof(ImageCell));
             cell.SetBinding(ImageCell.TextProperty, "Key");
             cell.SetBinding(ImageCell.DetailProperty, "Value");
 
-            var listView = new ListView{
+            var listView = new ListView
+            {
                 ItemsSource = item,
                 ItemTemplate = cell
             };
 
-            var addbutton = new Button{
+            var addbutton = new Button
+            {
                 HorizontalOptions = LayoutOptions.FillAndExpand,
                 Text = "+",
                 FontSize = 30,
@@ -46,13 +47,17 @@ namespace madaarumk2 {
                 Command = new Command(async () => {
                     string nextPageResult = await DisplayActionSheet("追加方法を選ぶ", "閉じる", "キャンセル", new string[] { "リストから", "スキャンする" });
 
-                    if (nextPageResult == "リストから"){//リストから選ぶページを表示
+                    if (nextPageResult == "リストから")
+                    {//リストから選ぶページを表示
                         await Navigation.PushAsync(new ChoiceFromBoughtThingPage(), true);
-                    }else if (nextPageResult == "スキャンする"){//スキャンするページを表示
-                         //await Navigation.PushAsync(new ScanBuyThingPage(), true);
-                         //ZXingのスキャナーを呼ぶ
+                    }
+                    else if (nextPageResult == "スキャンする")
+                    {//スキャンするページを表示
+                     //await Navigation.PushAsync(new ScanBuyThingPage(), true);
+                     //ZXingのスキャナーを呼ぶ
                         string scanedJancode = "";
-                        var scanPage = new ZXingScannerPage(){
+                        var scanPage = new ZXingScannerPage()
+                        {
                             DefaultOverlayTopText = "バーコードを読み取ります",
                             DefaultOverlayBottomText = "",
                         };
@@ -68,8 +73,9 @@ namespace madaarumk2 {
                                 //GetObjects go = new GetObjects();
                                 string json = await go.GetItemJsonString(scanedJancode);
 
-                                if (scanedJancode != null){//jsonの内容をチェック
-                                     //jancodeを元に情報を作成
+                                if (scanedJancode != null)
+                                {//jsonの内容をチェック
+                                 //jancodeを元に情報を作成
                                     SearchedInfo thingInfo = go.GetItemObjectFromJson(json);
                                     Bought_thing bt = new Bought_thing();
                                     User userInfo = (User)Application.Current.Properties["user"];
@@ -80,21 +86,26 @@ namespace madaarumk2 {
 
                                     //登録に必要な情報を渡す
                                     await Navigation.PushAsync(new ConcernBuyThingPage(bt), true);
-                                }else{//jancode is null
+                                }
+                                else
+                                {//jancode is null
                                     DependencyService.Get<IMyFormsToast>().Show("サーバにデータがありません");
                                     //手入力画面に移行する.手入力ページが未実装なのでコメントアウト
                                     //await Navigation.PushAsync(new ManualInputBuyThingPage(), true);
                                 }
-                              });
-                            };
+                            });
+                        };
 
-                        }else{//閉じるかキャンセルが押されているので何もしない(留まる)
-                            DependencyService.Get<IMyFormsToast>().Show("追加をキャンセルします.");
-                        }
-                    })
-                };
+                    }
+                    else
+                    {//閉じるかキャンセルが押されているので何もしない(留まる)
+                        DependencyService.Get<IMyFormsToast>().Show("追加をキャンセルします.");
+                    }
+                })
+            };
 
-            var RefreshList = new Button{
+            var RefreshList = new Button
+            {
                 HorizontalOptions = LayoutOptions.FillAndExpand,
                 Text = "更新",
                 FontSize = 20,
@@ -103,14 +114,16 @@ namespace madaarumk2 {
                 Command = new Command(() => { setBuyList(); })
             };
 
-            for (int n = 0; n < buythingInfo.Count; n++){
+            for (int n = 0; n < buythingInfo.Count; n++)
+            {
                 item[buythingInfo[n].name] = buythingInfo[n].alias;
                 //item.Add(expendablesInfo[n].name, expendablesInfo[n].limit);
                 //await DisplayAlert("商品名", expendablesInfo[n].name, "OK");
                 //await DisplayAlert("次回購入予定日", expendablesInfo[n].limit, "OK");
             }
 
-            Content = new StackLayout{
+            Content = new StackLayout
+            {
                 Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0), // iOSのみ上部にマージンをとる
                 Children = {
                         new StackLayout {
